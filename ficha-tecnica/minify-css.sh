e7b30c323bf1ad6657ef216389369595a32f46e7
Robson <rniemeyer@proton.me>
2025-09-20 15:34:35 -0300

    update performance

--- ficha-tecnica/minify-css.sh
+++ ficha-tecnica/minify-css.sh
@@ -1,10 +1,35 @@
 #!/bin/sh
+# Minify one CSS file, output as .min.css in same directory
 
-src="./assets/css/style.css"
-dst="./assets/css/style.min.css"
+if [ $# -ne 1 ]; then
+  echo "Usage: $0 path/to/file.css" >&2
+  exit 1
+fi
 
-sed 's:/\*[^*]*\*[^/]*\*/::g' "$src" | \
+SRC=$1
+
+case $SRC in
+  *.css) ;;
+  *)
+    echo "Error: input must be a .css file" >&2
+    exit 1
+    ;;
+esac
+
+if [ ! -f "$SRC" ]; then
+  echo "Error: $SRC not found" >&2
+  exit 1
+fi
+
+# Separate directory and filename
+DIR=$(dirname -- "$SRC")
+BASE=$(basename -- "$SRC" .css)
+DST="${DIR}/${BASE}.min.css"
+
+sed 's:/\*[^*]*\*[^/]*\*/::g' "$SRC" | \
 tr -d '\n' | \
 tr -s ' \t' ' ' | \
 sed 's/ \?{\s*/{/g; s/ \?}\s*/}/g; s/ \?:\s*/:/g; s/ \?;\s*/;/g; s/ \?,\s*/,/g; s/;}/}/g' \
-> "$dst"
+> "$DST"
+
+echo "Minified: $DST"
